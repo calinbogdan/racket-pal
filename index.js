dfb06558794ec7e503103a53d1c1feccb78b5f5d
Calin Bogdan <calin.bogdan@around25.com>
2021-07-21 07:54:33 +0300

    fix port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const app = require("./src/app");
 
-app.listen(3000, () => {
-  console.log("Server listening on port 3000.");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}.`);
 });
